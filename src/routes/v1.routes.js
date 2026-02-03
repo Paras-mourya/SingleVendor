c952aad4a6a3b7ba21f67f256b2c72cf215a7531
Paras-mourya <parasmourya288@gmail.com>
2026-02-03 10:20:13 +0530

    customer emailtemplate

--- src/routes/v1.routes.js
+++ src/routes/v1.routes.js
@@ -19,6 +19,7 @@ import socialMediaRoutes from './socialMedia.routes.js';
 import employeeManagementRoutes from './employeeManagement.routes.js';
 import employeeAuthRoutes from './employeeAuth.routes.js';
 import adminEmailTemplateRoutes from './adminEmailTemplate.routes.js';
+import customerEmailTemplateRoutes from './customerEmailTemplate.routes.js';
 
 const router = express.Router();
 
@@ -45,6 +46,7 @@ router.use('/social-media', socialMediaRoutes);
 router.use('/admin/staff', employeeManagementRoutes);
 router.use('/employee/auth', employeeAuthRoutes);
 router.use('/admin/admin-template', adminEmailTemplateRoutes);
+router.use('/admin/customer-template', customerEmailTemplateRoutes);
 // Health check can also be versioned if needed, but usually kept root
 router.use('/health', healthRoutes);
 
